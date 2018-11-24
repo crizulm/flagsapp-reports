b232c1d93ad34de8a762167f9cc6184973d6cb3d
Santiago Marchisio <santimarchisio_94@hotmail.com>
2018-11-24 03:36:11 -0300

    Add validation to create get update operations

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,11 +4,17 @@ class ReportsController < ApplicationController
     @report = Report.new(total_request: 0, true_answer: 0,
                          false_answer: 0, flag: params[:flag_id])
     @report.save
-    render json: {data: 'create'}, status: :ok
+    if @report.save
+      render json: {data: 'Correct create'}, status: :ok
+    else
+      return render json: {data: 'Error create faile'}, status: 400
+    end
   end
 
   def update_result
     report = Report.find_by(flag: params[:token])
+    return render json: {data: 'Error report not found'}, status: 400 if report.nil?
+
     report.total_request = report.total_request + 1
     if params[:result]
       report.true_answer = report.true_answer + 1
@@ -16,25 +22,39 @@ class ReportsController < ApplicationController
       report.false_answer = report.false_answer + 1
     end
     report.save
-    render json: {data: 'update'}, status: :ok
+    if report.save
+      render json: {data: 'Correct update'}, status: :ok
+    else
+      return render json: {data: 'Error update faile'}, status: 400
+    end
   end
 
   def update_time
     report = Report.find_by(flag: params[:token])
+    return render json: {data: 'Error report not found'}, status: 400 if report.nil?
+
     report.total_time = report.total_time + params[:time]
-    report.save
-    render json: {data: 'update'}, status: :ok
+
+    if report.save
+      render json: {data: 'Correct update'}, status: :ok
+    else
+      return render json: {data: 'Error update faile'}, status: 400
+    end
   end
 
 
   def show_json
     report = Report.find_by(flag: params[:token])
+    return render json: {data: 'Error report not found'}, status: 400 if report.nil?
+
     json_report = get_json(report)
     render json: {data: json_report}, status: :ok
   end
 
   def show
     report = Report.find_by(flag: params[:token])
+    return render json: {data: 'Error report not found'}, status: 400 if report.nil?
+
     render json: report, status: :ok
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2018_11_17_192433) do
     t.integer "total_request"
     t.integer "true_answer"
     t.integer "false_answer"
-    t.integer "flag"
+    t.string "flag"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
