dac9084a9b8b6bce55ccc62082a26a32bcc8b2ce
Matias Crizul <crizulm@gmail.com>
2018-11-25 18:15:50 -0300

    Reports refactoring

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,7 +2,7 @@ class ReportsController < ApplicationController
 
   def create
     token = params[:token]
-    return render json: { error: 'The flag token is NULL' }, status: :bad_request if token.nil?
+    return render json: ['The flag token is NULL'], status: :bad_request if token.nil?
 
     @report = Report.new(total_request: 0, true_answer: 0,
                          false_answer: 0, total_time: 0,
@@ -11,14 +11,15 @@ class ReportsController < ApplicationController
     if @report.save
       render json: @report, status: :ok
     else
-      return render json: { error: 'Error in flag creation' }, status: :bad_request
+      errors = @report.errors.full_messages
+      render json: errors, status: :bad_request
     end
   end
 
   def show
     token = params[:token]
     report = Report.find_by(flag: token)
-    return render json: { error: 'The report for flag ' + token + ' not found' }, status: :bad_request if report.nil?
+    return render json: ['The report for flag ' + token + ' not found'], status: :bad_request if report.nil?
 
     render json: report, status: :ok
   end
@@ -26,7 +27,7 @@ class ReportsController < ApplicationController
   def show_json
     token = params[:token]
     report = Report.find_by(flag: token)
-    return render json: { error: 'The report for flag ' + token + ' not found' }, status: :bad_request if report.nil?
+    return render json: ['The report for flag ' + token + ' not found'], status: :bad_request if report.nil?
 
     json_report = get_json(report)
     render json: json_report, status: :ok
@@ -35,10 +36,10 @@ class ReportsController < ApplicationController
   def update_result
     token = params[:token]
     result = params[:result]
-    return render json: { error: 'The flag evaluate result is NULL' }, status: :bad_request if result.nil?
+    return render json: ['The flag evaluate result is NULL'], status: :bad_request if result.nil?
 
     report = Report.find_by(flag: token)
-    return render json: { error: 'The report for flag ' + token + ' not found' }, status: :bad_request if report.nil?
+    return render json: ['The report for flag ' + token + ' not found'], status: :bad_request if report.nil?
 
     report.total_request = report.total_request + 1
     if result
@@ -50,24 +51,26 @@ class ReportsController < ApplicationController
     if report.save
       render json: report, status: :ok
     else
-      return render json: { error: 'Error in flag update' }, status: :bad_request
+      errors = @report.errors.full_messages
+      render json: errors, status: :bad_request
     end
   end
 
   def update_time
     token = params[:token]
     time = params[:time]
-    return render json: { error: 'The flag evaluate time is NULL' }, status: :bad_request if time.nil?
+    return render json: ['The flag evaluate time is NULL'], status: :bad_request if time.nil?
 
     report = Report.find_by(flag: token)
-    return render json: { error: 'The report for flag ' + token + ' not found' }, status: :bad_request if report.nil?
+    return render json: ['The report for flag ' + token + ' not found'], status: :bad_request if report.nil?
 
     report.total_time = report.total_time + time
 
     if report.save
       render json: report, status: :ok
     else
-      return render json: { error: 'Error in flag update' }, status: :bad_request
+      errors = @report.errors.full_messages
+      render json: errors, status: :bad_request
     end
   end
 
